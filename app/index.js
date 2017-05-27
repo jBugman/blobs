101460e493c735c4cfb20ebdab7a46b2d4f3f0fd
Sergey Parshukov <codedby@bugman.me>
2017-05-27 22:12:00 +0300

    CSS reset

--- app/index.js
+++ app/index.js
@@ -1,8 +1,20 @@
-import {utils} from 'pixi.js'
+import 'normalize.css'
+// import {utils, autoDetectRenderer, Container} from 'pixi.js'
+import * as PIXI from 'pixi.js'
 
 var type = 'WebGL'
-if (!utils.isWebGLSupported()) {
+if (!PIXI.utils.isWebGLSupported()) {
   type = 'canvas'
 }
+PIXI.utils.sayHello(type)
 
-utils.sayHello(type)
+var renderer = PIXI.autoDetectRenderer(256, 256)
+document.body.appendChild(renderer.view)
+renderer.backgroundColor = 0xffffff
+renderer.view.style.position = 'absolute'
+renderer.view.style.display = 'block'
+renderer.autoResize = true
+renderer.resize(window.innerWidth, window.innerHeight)
+
+var stage = new PIXI.Container()
+renderer.render(stage)
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,15 @@ module.exports = {
   entry: './app/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   }
 }
