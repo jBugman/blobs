de9e237f2a3912e5692f8578e9405852a26f6eb5
Sergey Parshukov <codedby@bugman.me>
2017-05-28 18:49:18 +0300

    Physics-based movement with matter.js

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import 'normalize.css'
 import * as PIXI from 'pixi.js'
 import mousePosition from 'mouse-position'
-import Victor from 'victor'
+import {Engine, Bodies, World, Body, Vector} from 'matter-js'
 
 const mouse = mousePosition()
 
@@ -16,16 +16,26 @@ document.body.appendChild(app.view)
 app.stage.interactive = true
 app.stage.hitArea = app.screen
 
+const engine = Engine.create()
+engine.world.gravity.y = 0
+var bodies = []
+
+// engine.world.bounds = {min: {x: 0, y: 0}, max: {x: app.screen.width, y: app.screen.height}}
+// var wall1 = Bodies.rectangle(0, app.screen.height, app.screen.width, 10, {isStatic: true})
+// bodies.push(wall1)
+
 const player = new PIXI.Graphics()
+player.radius = 50
 player.beginFill(0x429bf4)
 player.lineStyle(4, 0x2e6caa, 1)
-player.drawCircle(0, 0, 50)
-player.v = new Victor(0, 0)
+player.drawCircle(0, 0, player.radius)
 player.endFill()
-player.interactive = true
-player.position.x = app.screen.width / 2
-player.position.y = app.screen.height / 2
 app.stage.addChild(player)
+player.interactive = true
+player.body = Bodies.circle(app.screen.width / 2, app.screen.height / 2, player.radius)
+player.body.friction = 0
+player.body.frictionAir = 0
+bodies.push(player.body)
 
 app.stage.on('click', function (event) {
   if (event.target === player) {
@@ -33,16 +43,18 @@ app.stage.on('click', function (event) {
     return
   }
   // relative click direction -> speed vector
-  player.v = new Victor(mouse[0] - player.x, mouse[1] - player.y).normalize()
+  var v = Vector.create(mouse[0] - player.x, mouse[1] - player.y)
+  Body.setVelocity(player.body, Vector.normalise(v))
 })
 
 function gameLoop () {
   window.requestAnimationFrame(gameLoop)
 
-  player.x += player.v.x
-  player.y += player.v.y
+  player.position = player.body.position
 
   app.renderer.render(app.stage)
 }
 
 gameLoop()
+World.add(engine.world, bodies)
+Engine.run(engine)
